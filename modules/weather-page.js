2afd3f9bc9a4f5564c183b853feec0ad63121784
Joseph Kessler <kesslerjx@icloud.com>
2022-05-27 11:37:02 -0400

    Add form validation and functionality to the go button

--- modules/weather-page.js
+++ modules/weather-page.js
@@ -4,10 +4,22 @@ import { getWeatherData } from "./handlers/weather-data-handler.js";
 const inputField = document.querySelector('#zip-code-input');
 const goButton = document.querySelector('#go-button');
 
-
 //Loads the page with its initial content
 function loadPage() {
-    getWeatherData('20657');
+
+    goButton.addEventListener('click', validateForm);
+
+}
+
+//Ensures that the input is a valid US zipcode
+function validateForm() {
+    if(/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(inputField.value)) {
+        console.log("Valid zip code");
+        getWeatherData(inputField.value);
+    } else {
+        console.log("Invalid zip code.");
+        inputField.value = ""; //Clears the field
+    }
 }
 
 //Puts the weather data in their respective elements
